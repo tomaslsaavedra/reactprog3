73879c653c02156b7b2b687b29438724bcd5f777
Manuel Firpo <manufirpo@hotmail.com>
2021-04-24 14:56:49 -0300

    probando

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import { Component } from "react";
 
 export default class App extends Component {
 
+  agregar() {
+    console.log("agregar")
+  }
+
+  borrar() {
+    console.log("borrar")
+  }
+
   render(){
   return (
     <div className="App">
       <div class="botones">
-        <button>Agregar una tarjeta mas</button>
-        <button>Borrar todas las tarjetas</button>
+        <button onClick={this.agregar.bind(this)}>Agregar una tarjeta mas</button>
+        <button onClick={this.borrar.bind(this)}>Borrar todas las tarjetas</button>
       </div>
       <Main/>
       <Main/>
