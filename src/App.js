0799e486fcca6127ae8ed6d98b404f37363ab39b
Agustin <agustingonzalezcutre@gmail.com>
2021-04-24 15:13:46 -0300

    api

--- src/App.js
+++ src/App.js
@@ -4,20 +4,59 @@ import { Component } from "react";
 
 export default class App extends Component {
 
-  agregar() {
-    console.log("agregar")
-  }
+    constructor() {
+      super();
+      this.state = {
+        items: [],
+      }
+    }
 
-  borrar() {
-    console.log("borrar")
+  componentDidMount(){
+    
+      fetch('https://randomuser.me/api/?results=10 ')
+      .then(result=>result.json())
+      .then(data=> {
+        this.setState({items:data.results});
+        console.log(data.results);
+      })
+    
+      
+    
   }
+      
+
+
+    agregar() {
+
+      fetch('https://randomuser.me/api/')
+      .then(result=>result.json())
+      .then(data=> {
+        this.state.items.push(data.results[0]);
+        this.setState({items: this.state.items});
+        console.log(data.results);
+
+    
+    })
+    }
+
+    borrar(idTarjeta){
+      let resultado = this.state.items.filter((item)=>{
+          return item.login.uuid !== idTarjeta;
+        })
+        this.setState({items:resultado})
+      console.log("tarjeta a borrar:"+ idTarjeta);
+    }
+    
+    borrartodas () {
+      this.setState({items:[]});
+    }
 
   render(){
   return (
     <div className="App">
       <div class="botones">
         <button onClick={this.agregar.bind(this)}>Agregar una tarjeta mas</button>
-        <button onClick={this.borrar.bind(this)}>Borrar todas las tarjetas</button>
+        <button onClick={this.borrartodas.bind(this)}>Borrar todas las tarjetas</button>
       </div>
       <Main/>
       <Main/>
--- src/component/Main.js
+++ src/component/Main.js
@@ -17,12 +17,12 @@ export default class Child extends Component {
     return (
         <div class="container">
             <div class="card" style={{borderColor: this.state.color}} onMouseEnter={this.cambiodeColor.bind (this,"green")} onMouseLeave= {this.cambiodeColor.bind (this,'black')}>
-                <div> Tomas </div>
+                <div> Cutre </div>
                 <div> tomas@lopezsaavedra.com.ar </div>
                 <div>
                     <img src="https://i1.sndcdn.com/avatars-000219066532-yudgtg-t500x500.jpg" height="100" width="auto"></img>
                 </div>
-                <div> 22 años</div>
+                <div> 666 años</div>
                 <div> 
                     <div> Buenos Aires </div>
                     <div> +54 9 11 2185-0815 </div>
